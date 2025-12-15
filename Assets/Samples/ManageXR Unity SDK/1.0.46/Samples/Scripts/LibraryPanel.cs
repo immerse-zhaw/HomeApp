bc396f46d7b803d5bf048765f2c149ab229130ef
Zobaer Ahmed Salam <zobaer.ahmed.salam@gmail.com>
2025-12-15 18:16:06 +0100

    fixed UI

--- Assets/Samples/ManageXR Unity SDK/1.0.46/Samples/Scripts/LibraryPanel.cs
+++ Assets/Samples/ManageXR Unity SDK/1.0.46/Samples/Scripts/LibraryPanel.cs
@@ -10,10 +10,25 @@ namespace MXR.SDK.Samples {
     // this script destroys the previously instantiated cells and instantiates 
     // them again. Not efficient, we know!. But this is just a demo.
     public class LibraryPanel : MonoBehaviour {
-        [SerializeField] Transform cellContainer;
+        [Header("Scroll View")]
+        [SerializeField] ScrollRect scrollRect;
+        
+        [Header("Content Containers")]
+        [SerializeField] GameObject appsContainer;
+        [SerializeField] GameObject videosContainer;
+        [SerializeField] GameObject webXRContainer;
+
+        [Header("Buttons")]
+        [SerializeField] Button appsButton;
+        [SerializeField] Button videosButton;
+        [SerializeField] Button webXRButton;
+
+        [Header("Cell Templates")]
         [SerializeField] RuntimeAppCell appCellTemplate;
         [SerializeField] WebXRAppCell webXRAppCellTemplate;
         [SerializeField] VideoCell videoCellTemplate;
+        
+        [Header("Error UI")]
         [SerializeField] GameObject errPanel;
         [SerializeField] Text errLabel;
 
@@ -28,18 +43,59 @@ namespace MXR.SDK.Samples {
             webXRAppCellTemplate.gameObject.SetActive(false);
             videoCellTemplate.gameObject.SetActive(false);
 
+            // Setup button listeners
+            if (appsButton != null) appsButton.onClick.AddListener(() => ShowContent(ContentType.Apps));
+            if (videosButton != null) videosButton.onClick.AddListener(() => ShowContent(ContentType.Videos));
+            if (webXRButton != null) webXRButton.onClick.AddListener(() => ShowContent(ContentType.WebXR));
+
             OnRuntimeSettingsSummaryChange(MXRManager.System.RuntimeSettingsSummary);
             OnDeviceStatusChange(MXRManager.System.DeviceStatus);
 
             MXRManager.System.OnRuntimeSettingsSummaryChange += OnRuntimeSettingsSummaryChange;
             MXRManager.System.OnDeviceStatusChange += OnDeviceStatusChange;
              
+            // Show apps by default
+            ShowContent(ContentType.Apps);
+            
             Debug.Log("The system infor");
         }
 
         void OnDestroy() {
             MXRManager.System.OnRuntimeSettingsSummaryChange -= OnRuntimeSettingsSummaryChange;
             MXRManager.System.OnDeviceStatusChange -= OnDeviceStatusChange;
+            
+            if (appsButton != null) appsButton.onClick.RemoveAllListeners();
+            if (videosButton != null) videosButton.onClick.RemoveAllListeners();
+            if (webXRButton != null) webXRButton.onClick.RemoveAllListeners();
+        }
+
+        public enum ContentType {
+            Apps,
+            Videos,
+            WebXR
+        }
+
+        public void ShowContent(ContentType type) {
+            if (appsContainer != null) appsContainer.SetActive(type == ContentType.Apps);
+            if (videosContainer != null) videosContainer.SetActive(type == ContentType.Videos);
+            if (webXRContainer != null) webXRContainer.SetActive(type == ContentType.WebXR);
+            
+            // Update ScrollRect content reference
+            if (scrollRect != null) {
+                switch (type) {
+                    case ContentType.Apps:
+                        if (appsContainer != null) scrollRect.content = appsContainer.GetComponent<RectTransform>();
+                        break;
+                    case ContentType.Videos:
+                        if (videosContainer != null) scrollRect.content = videosContainer.GetComponent<RectTransform>();
+                        break;
+                    case ContentType.WebXR:
+                        if (webXRContainer != null) scrollRect.content = webXRContainer.GetComponent<RectTransform>();
+                        break;
+                }
+            }
+            
+            Debug.Log($"[LibraryPanel] Showing {type}");
         }
 
         void OnRuntimeSettingsSummaryChange(RuntimeSettingsSummary obj) {
@@ -79,9 +135,11 @@ namespace MXR.SDK.Samples {
         }
 
         void InstantiateWebXRCells() {
+            if (webXRContainer == null) return;
+            
             MXRManager.System.RuntimeSettingsSummary.webXRApps.Values.ToList()
                 .ForEach(x => {
-                    var instance = Instantiate(webXRAppCellTemplate, cellContainer);
+                    var instance = Instantiate(webXRAppCellTemplate, webXRContainer.transform);
                     instance.gameObject.SetActive(true);
                     instance.gameObject.name = x.title;
                     instance.webXRApp = x;
@@ -91,9 +149,11 @@ namespace MXR.SDK.Samples {
         }
 
         void InstantaiteVideoCells() {
+            if (videosContainer == null) return;
+            
             MXRManager.System.RuntimeSettingsSummary.videos.Values.ToList()
                 .ForEach(x => {
-                    var instance = Instantiate(videoCellTemplate, cellContainer);
+                    var instance = Instantiate(videoCellTemplate, videosContainer.transform);
                     instance.gameObject.SetActive(true);
                     instance.gameObject.name = x.title;
                     instance.video = x;
@@ -104,9 +164,11 @@ namespace MXR.SDK.Samples {
         }
 
         void InstantiateAppCells() {
+            if (appsContainer == null) return;
+            
             MXRManager.System.RuntimeSettingsSummary.apps.Values.ToList()
                 .ForEach(x => {
-                    var instance = Instantiate(appCellTemplate, cellContainer);
+                    var instance = Instantiate(appCellTemplate, appsContainer.transform);
                     instance.gameObject.SetActive(true);
                     instance.gameObject.name = x.title;
                     instance.runtimeApp = x;
